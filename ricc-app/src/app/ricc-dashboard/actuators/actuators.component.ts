86842f1af032527f3d15062a43e5693265d02e91
Hugo Carvalho <hugonvsc@gmail.com>
2019-06-30 23:20:13 -0300

    Add actuators view

--- ricc-app/src/app/ricc-dashboard/actuators/actuators.component.ts
+++ ricc-app/src/app/ricc-dashboard/actuators/actuators.component.ts
@@ -15,9 +15,13 @@ export class ActuatorsComponent implements OnInit {
   authToken: string;
   actuators: Array<Actuator>;
   relatedCentral: string;
+  changeStatusText: string;
+  centrals: Array<string>;
 
   constructor(private http: HttpClient, private router: Router, private route: ActivatedRoute) {
+    this.centrals = [];
     this.actuators = [];
+    this.changeStatusText = "";
    }
 
    ngOnInit() {
@@ -43,20 +47,86 @@ export class ActuatorsComponent implements OnInit {
         for (const actuator of data['actuators']) {
           if (this.relatedCentral !== null){
             if (this.relatedCentral == actuator.related_central){
-              this.actuators.push(actuator);
+              actuators.push(actuator);
+              this.getCentrals(actuator);
             }
           } else{
-            this.actuators.push(actuator);
+            actuators.push(actuator);
+            this.getCentrals(actuator);
           }
-        }  
+        }
+        this.addDataActuators(actuators);
       }, 
       err => {
         console.log("Not good enough!");
         
       }
     );
-
-    return actuators;
   }
 
+  getCentrals(station){
+    // Get centrals 
+    if(this.centrals.indexOf(station.related_central) == -1 ){
+      this.centrals.push(station.related_central);            
+    }
+  }
+
+  addDataActuators(actuators){
+    for (const central of this.centrals) {      
+
+      let args = {
+        auth_token: this.authToken,
+        central: central
+      }
+
+      this.http.post(BaseURL + 'actuator/last_datas/', args)
+      .subscribe(
+        data => {
+          let datas = data as Array<any>;
+          
+          for (const d of datas) {
+            let actuatorName = d['actuator']['name'];
+            let actuatorData = d['data'];
+
+            let actuator = actuators.find(a => a.name === actuatorName);            
+            actuator.data = actuatorData;
+            this.actuators.push(actuator);            
+          }          
+        }, 
+        err => {
+          console.log("Not good enough!");
+          
+        }
+      );        
+    }
+
+    console.log(this.actuators);
+    
+
+  }
+
+  switchActuator(central){
+    let message = {
+      central: central,
+      auth_token: this.authToken
+    };  
+    
+    console.log("asdf", message);
+  
+    this.http.post(BaseURL + 'actuator/switch/', message)
+    .subscribe(
+      data => {
+        console.log("Alterou", data);
+        
+      }, 
+      err => {
+        console.log(err);
+        
+        this.changeStatusText = "Um erro inesperado aconteceu!";
+      }
+    ); 
+  
+  }
 }
+
+
