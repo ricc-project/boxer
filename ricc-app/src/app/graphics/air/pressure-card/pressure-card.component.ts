b34dcfaf3d5613a82c0951cbf37a577281e394a6
Hugo Carvalho <hugonvsc@gmail.com>
2019-07-01 15:16:13 -0300

    Add pressure card

--- ricc-app/src/app/graphics/air/pressure-card/pressure-card.component.ts
+++ ricc-app/src/app/graphics/air/pressure-card/pressure-card.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-pressure-card',
+  selector: 'air-pressure-card',
   templateUrl: './pressure-card.component.html',
   styleUrls: ['./pressure-card.component.scss']
 })
 export class PressureCardComponent implements OnInit {
+  @Input('value') value: number;
 
   constructor() { }
 
