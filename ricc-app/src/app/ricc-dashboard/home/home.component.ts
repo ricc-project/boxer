0ca7c77d9f6f3b108fd4d09aeca69d75be65279d
Hugo Carvalho <hugonvsc@gmail.com>
2019-07-09 14:47:56 -0300

    Add dragable

--- ricc-app/src/app/ricc-dashboard/home/home.component.ts
+++ ricc-app/src/app/ricc-dashboard/home/home.component.ts
@@ -7,6 +7,7 @@ import { GraphComponent } from '../../models/graph-types';
 import { NgForm } from '@angular/forms';
 import { Station } from '../../models/station';
 import { Central } from '../../models/central';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-home',
@@ -23,7 +24,17 @@ export class HomeComponent implements OnInit, AfterViewInit {
   graphComponent : GraphComponent;
   r: Array<any>;
   loadingText: string;
-  a: string = "ASDF"
+
+  movies = [
+    'Episode I - The Phantom Menace',
+    'Episode II - Attack of the Clones',
+    'Episode III - Revenge of the Sith',
+    'Episode IV - A New Hope',
+    'Episode V - The Empire Strikes Back',
+    'Episode VI - Return of the Jedi',
+    'Episode VII - The Force Awakens',
+    'Episode VIII - The Last Jedi'
+  ];
 
 
   @ViewChildren('cardView', {read: ViewContainerRef}) viewChilren: QueryList<ViewContainerRef>;
@@ -80,4 +91,7 @@ export class HomeComponent implements OnInit, AfterViewInit {
     }
   }
 
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.cards, event.previousIndex, event.currentIndex);
+  }
 }
